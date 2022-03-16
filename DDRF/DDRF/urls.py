3ff4c01f382b0a770eb6417d818fc1a2f74fe8ca
hyeonukdev <lego7407@gmail.com>
2022-03-16 22:19:57 +0900

    :sparkles: Add posts App

--- DDRF/DDRF/urls.py
+++ DDRF/DDRF/urls.py
@@ -1,6 +1,7 @@
 from django.urls import include, path
 from rest_framework import routers
 from users import views
+from django.urls import path, include
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
@@ -9,6 +10,7 @@ router.register(r'groups', views.GroupViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    # path('', include(router.urls)),
+    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('post-api/', include('posts.urls')),
 ]
